4643d48b7c60bead15b5390c5b53c9d750236866
AlbearKamoo <mrautran@gmail.com>
2016-05-13 15:44:59 -0700

    Pushing to pull

---
+++ app/src/main/java/com/example/whatsemo/classmates/fragment/NewGroupFragment.java
@@ -0,0 +1,10 @@
+package com.example.whatsemo.classmates.fragment;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by marcelo on 5/13/2016.
+ */
+public class NewGroupFragment extends DialogFragment {
+
+}
