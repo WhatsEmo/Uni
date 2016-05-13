2965286482a9ebcedd10b0f2a1a7787592bf703f
WhatsEmo <bscheng@uci.edu>
2016-05-12 21:15:29 -0700

    test

--- app/src/main/java/com/example/whatsemo/classmates/NotificationActivity.java
+++ app/src/main/java/com/example/whatsemo/classmates/NotificationActivity.java
@@ -8,6 +8,8 @@ import com.firebase.client.Firebase;
 
 import java.util.ArrayList;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by WhatsEmo on 5/8/2016.
  */
@@ -19,6 +21,8 @@ public class NotificationActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.notifications_layout);
+        ButterKnife.bind(this);
 
         firedata = new Firebase(getString(R.string.database));
     }
