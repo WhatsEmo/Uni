364757ac591f4492b02bf2e7e4887202a7c966c9
AlbearKamoo <mrautran@gmail.com>
2016-05-13 23:27:14 -0700

    groups are cool

--- app/src/main/java/com/example/whatsemo/classmates/fragment/NewGroupFragment.java
+++ app/src/main/java/com/example/whatsemo/classmates/fragment/NewGroupFragment.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import android.widget.SearchView;
 
 import com.example.whatsemo.classmates.R;
+import com.firebase.client.Firebase;
+
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -30,6 +33,16 @@ public class NewGroupFragment extends DialogFragment {
     @Bind(R.id.friends_result_view)
     RecyclerView recyclerView;
 
+    @OnClick(R.id.confirm_group_button)
+    public void confirmGroup(){
+        addToDatabase();
+    }
+
+    private String groupName;
+    private HashMap<String, String> members;
+
+    private Firebase firedata;
+
     public NewGroupFragment(){
         // Required empty public constructor
     }
@@ -44,11 +57,27 @@ public class NewGroupFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firedata = new Firebase(getResources().getString(R.string.database));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.new_group_layout, container, false);
+
+        ButterKnife.bind(this, view);
+
         return view;
+    }
+
+    private void addToDatabase(){
+        Firebase groupRef = firedata.child("groups").push();
+
+        groupName = newGroupName.getText().toString();
+
+        if(!groupName.isEmpty()) {
+            firedata.child("groups").child(groupRef.getKey()).child("name").setValue(groupName);
+            firedata.child("groups").child(groupRef.getKey()).child("members").setValue(members);
+            NewGroupFragment.this.dismiss();
+        }
     }
 }
